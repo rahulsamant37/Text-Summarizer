880f25e7292550bca78ea5ad8177a20100b58206
rahulsamant37 <161972011+rahulsamant37@users.noreply.github.com>
2024-12-29 14:54:11 +0000

    setting-up src\pipeline\data_transformation_pipeline.py for data transformation

---
+++ src/pipeline/data_transformation_pipeline.py
@@ -0,0 +1,15 @@
+from src.config.configuration import ConfigurationManager
+from src.components.Data_Transformation import DataTransformation
+from src import logger
+
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_transformation(self):
+        config=ConfigurationManager()
+        data_transformation_config=config.get_data_transformation_config()
+        data_transformation=DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
+        
