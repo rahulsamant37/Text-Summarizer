be82f10f74aaffdb53cdc1f8322802f8d8c629b4
rahulsamant37 <161972011+rahulsamant37@users.noreply.github.com>
2024-12-29 15:11:07 +0000

    setting-up src/cpipeline for model training

---
+++ src/pipeline/model_trainer_pipeline.py
@@ -0,0 +1,12 @@
+from src.config.configuration import ConfigurationManager
+from src.components.Model_Trainer import ModelTrainer
+from src import logger
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+    def initiate_model_trainer(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
