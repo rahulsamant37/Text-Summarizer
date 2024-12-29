f58997ed48780fdc1a1454d592a7429815046168
rahulsamant37 <161972011+rahulsamant37@users.noreply.github.com>
2024-12-29 15:21:57 +0000

    adding src/pipeline for model evaluation

---
+++ src/pipeline/model_evaluation_pipeline.py
@@ -0,0 +1,13 @@
+from src.config.configuration import ConfigurationManager
+from src.components.Model_Evaluation import ModelEvaluation
+from src import logger
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_model_evaluation(self):
+        config=ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation= ModelEvaluation(config=model_evaluation_config)
+        model_evaluation.evaluate()
